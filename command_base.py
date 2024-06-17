6afa8fb851d72e46e324938ce27236622e2ef492
user71424q <127671064+user71424q@users.noreply.github.com>
2024-06-17 17:23:27 +0300

    Delete command_base.py

--- command_base.py
+++
@@ -1,6 +0,0 @@
-class Command:
-    command_description: dict = {}
-
-    @classmethod
-    def execute(cls, text: str) -> None | str:
-        raise NotImplementedError("Команда должна реализовывать метод execute()")
